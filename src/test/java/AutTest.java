199083608f694d71933fb0e85ba700eae8bf15d4
Nicroft <ortiznicolas28@gmail.com>
2022-03-29 21:10:54 -0500

    nomeda:c

---
+++ src/test/java/AutTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AutTest {
+
+    private WebDriver driver;
+
+
+    @BeforeEach
+   public void setUp() {
+        System.setProperty("webdriver.edge.driver","src/test/resources/EdgeDriver/msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    @Test
+    public void shouldPromptErrorWhenUserAndPasswordAreIncorrect() {
+
+        WebElement user = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        user.clear();
+        password.clear();
+
+        user.sendKeys("admin");
+        password.sendKeys("admin");
+
+        loginButton.click();
+
+        WebElement flash = driver.findElement(By.id("flash"));
+
+        String flashClass = flash.getAttribute ("class");
+
+        assertEquals("flash error", flashClass);
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        //driver.quit();
+    }
+}
